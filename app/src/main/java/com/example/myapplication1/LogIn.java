7ef45ffbb32f6505ebaa6f28a3c9235bd0cf15ec
miruna.cojocaru <servetelezewa34>
2020-11-02 15:48:08 +0200

    try to take a invoice list from api. doesn't wok yet

--- app/src/main/java/com/example/myapplication1/LogIn.java
+++ app/src/main/java/com/example/myapplication1/LogIn.java
@@ -8,16 +8,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.myapplication1.Model.UserLogIn;
 import com.example.myapplication1.Remote.APIInterfaces;
 import com.example.myapplication1.Remote.RetrofitClientLogIn;
-
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Consumer;
@@ -39,9 +36,6 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
-
-
         saveData =(TextView) findViewById(R.id.tvSave) ;
         restoreData = (TextView) findViewById(R.id.tvRemember);
 
@@ -50,18 +44,17 @@ public class LogIn extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 SharedPreferences sharedPreferences = getSharedPreferences("User info", Context.MODE_PRIVATE);
+
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("user", userName.getText().toString());
                 editor.putString("password", userPassword.getText().toString());
                 editor.apply();
 
-                Toast.makeText(getApplicationContext(), "Saved!", Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(getApplicationContext(), "Salvat!", Toast.LENGTH_SHORT).show();
             }
         });
 
         restoreData.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 SharedPreferences sharedPreference= getSharedPreferences("User info", Context.MODE_PRIVATE);
@@ -75,7 +68,6 @@ public class LogIn extends AppCompatActivity {
 
         //initializare api
         logInAPI = RetrofitClientLogIn.getInstance().create(APIInterfaces.class);
-
         userName = findViewById(R.id.userName);
         userPassword = findViewById(R.id.userPassword);
 
@@ -86,23 +78,72 @@ public class LogIn extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-
-
                 UserLogIn user = new UserLogIn(userName.getText().toString(), userPassword.getText().toString());
                 compositeDisposable.add(logInAPI.loginUser(user).subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<String>() {
                             @Override
                             public void accept(String s) throws Exception {
-                                Toast.makeText(LogIn.this, s, Toast.LENGTH_LONG).show();
+
+                                Toast.makeText(LogIn.this, "Conectare...", Toast.LENGTH_LONG).show();
                                 Intent intentAutentificare = new Intent(LogIn.this, MainActivity.class);
                                 startActivity(intentAutentificare);
-
                             }
                         }, new Consumer<Throwable>() {
                             @Override
                             public void accept(Throwable throwable) throws Exception {
-                                Toast.makeText(LogIn.this, throwable.getMessage(), Toast.LENGTH_LONG).show();
+                                if (throwable.getMessage().equals("HTTP 404 Not Found")) {
+                                    builder = new AlertDialog.Builder(LogIn.this);
 
+                                    inflater = LayoutInflater.from(LogIn.this);
+                                    View view = inflater.inflate(R.layout.custom_alert_dialog, null);
+
+                                    Button noButton = view.findViewById(R.id.button_cancel);
+                                    Button yesButton = view.findViewById(R.id.button_reload);
+
+                                    builder.setView(view);
+                                    dialogError = builder.create();
+                                    dialogError.show();
+
+                                    yesButton.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            dialogError.dismiss();
+                                        }
+                                    });
+                                    noButton.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            android.os.Process.killProcess(android.os.Process.myPid());
+                                        }
+                                    });
+                                } else {
+                                    if (!throwable.getMessage().equals("HTTP 404 Not Found") && throwable.getMessage().equals("200")) {
+                                        builder = new AlertDialog.Builder(LogIn.this);
+
+                                        inflater = LayoutInflater.from(LogIn.this);
+                                        View view = inflater.inflate(R.layout.custom_network_dialog, null);
+
+                                        Button ntnoButton = view.findViewById(R.id.button_ntcancel);
+                                        Button ntyesButton = view.findViewById(R.id.button_ntreload);
+
+                                        builder.setView(view);
+                                        dialogNetwork = builder.create();
+                                        dialogNetwork.show();
+
+                                        ntyesButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                dialogNetwork.dismiss();
+                                            }
+                                        });
+                                        ntnoButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                android.os.Process.killProcess(android.os.Process.myPid());
+                                            }
+                                        });
+                                    }
+                                }
                             }
                         }));
             }
