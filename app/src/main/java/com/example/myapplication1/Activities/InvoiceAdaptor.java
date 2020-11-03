f51546f741e00e2ea21116285b5fb15a0691fe9a
ileanazina <69892808+ileanazina@users.noreply.github.com>
2020-11-03 16:39:46 +0200

    invoice designe

--- app/src/main/java/com/example/myapplication1/Activities/InvoiceAdaptor.java
+++ app/src/main/java/com/example/myapplication1/Activities/InvoiceAdaptor.java
@@ -1,11 +1,13 @@
 package com.example.myapplication1.Activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,35 +24,33 @@ public class InvoiceAdaptor extends RecyclerView.Adapter<InvoiceAdaptor.ExempleV
     private Context mContext;
     private List<InvoiceModel> mList;
     private OnInvoiceListener monInvoiceListener;
-    public AccountModel account;
+
 
     public InvoiceAdaptor(Context mContext, List<InvoiceModel> mList,OnInvoiceListener onInvoiceListener) {
         this.mContext = mContext;
         this.mList = mList;
         this.monInvoiceListener=onInvoiceListener;
 
-        SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this.mContext);
-        Gson gson = new Gson();
-        String json = mPrefs.getString("AccountInfo",null);
-        account = gson.fromJson(json, AccountModel.class);
     }
 
     @NonNull
     @Override
     public ExempleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(mContext).inflate(R.layout.invoice_recycle_view,parent,false);
+        View v = LayoutInflater.from(mContext).inflate(R.layout.invoice_row_view,parent,false);
         return new ExempleViewHolder(v,monInvoiceListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ExempleViewHolder holder, int position) {
         InvoiceModel item = mList.get(position);
-        if(account.getAccountId() == item.getAccountId()) {
+
             SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
             String dataDue = formatter.format(item.getDueDate());
-            String aux = "Numar facura: " + item.getInvoiceId() + "\nTotal de plata: " + item.getValueWithVat() + "\nData scadenta: " + dataDue;
-            holder.textView.setText(aux);
-        }
+            //String aux = "Numar facura: " + item.getInvoiceId() + "\nTotal de plata: " + item.getValueWithVat() + "\nData scadenta: " + dataDue;
+            holder.tvInvoiceNr.setText("Numar facura: " + item.getInvoiceId());
+            holder.tvTotalPay.setText("Total de plata: " + item.getValueWithVat());
+            holder.tvDueDate.setText("Data scadenta: " + dataDue);
+
     }
 
     @Override
@@ -59,21 +59,67 @@ public class InvoiceAdaptor extends RecyclerView.Adapter<InvoiceAdaptor.ExempleV
     }
 
     public class ExempleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        public TextView textView;
+        public TextView tvInvoiceNr,tvTotalPay, tvDueDate;
+        public Button viewInfo,payInvoice;
         OnInvoiceListener onInvoiceListener;
 
         public ExempleViewHolder(@NonNull View itemView, OnInvoiceListener onInvoiceListener) {
             super(itemView);
-            textView = itemView.findViewById(R.id.invoice_recycle_text);
+            tvInvoiceNr = itemView.findViewById(R.id.invoiceNr);
+            tvTotalPay = itemView.findViewById(R.id.toaltoPay);
+            tvDueDate = itemView.findViewById(R.id.dataDue);
+
+
+            viewInfo = itemView.findViewById(R.id.Viewbtn);
+            payInvoice = itemView.findViewById(R.id.Paybtn);
+
+            viewInfo.setOnClickListener(this);
+            payInvoice.setOnClickListener(this);
+
             this.onInvoiceListener=onInvoiceListener;
             itemView.setOnClickListener(this);
         }
 
+//        @Override
+//        public void onClick(View v) {
+//            onInvoiceListener.onInvoiceListener(getAdapterPosition());
+//        }
         @Override
         public void onClick(View v) {
             onInvoiceListener.onInvoiceListener(getAdapterPosition());
+            int position;
+            position = getAdapterPosition();
+            InvoiceModel item = mList.get(position);
+
+            switch (v.getId()) {
+                case R.id.Viewbtn:
+                    viewInvoice(item.getInvoiceId());
+                    break;
+                case R.id.Paybtn:
+                    paingInvoice(item.getInvoiceId());
+                    break;
+            }
+
+        }
+        private void viewInvoice(final int id)
+        {
+            Intent intentDetais = new Intent(mContext , InvoiceDetails.class);
+            intentDetais.putExtra("extra", id);
+            intentDetais.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intentDetais);
+
+        }
+
+        private void paingInvoice(final int id)
+        {
+            Intent intentPay = new Intent(mContext , Payments.class);
+            intentPay.putExtra("extra", id);
+            intentPay.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intentPay);
         }
     }
+
+
 
     public interface OnInvoiceListener {
         void onInvoiceListener(int position);
--- app/src/main/java/com/example/myapplication1/Activities/Invoices.java
+++ app/src/main/java/com/example/myapplication1/Activities/Invoices.java
@@ -3,22 +3,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+
+
+
+import com.example.myapplication1.Model.AccountModel;
 import com.example.myapplication1.Model.InvoiceModel;
 import com.example.myapplication1.R;
 import com.example.myapplication1.Remote.APIInterfaces;
 import com.example.myapplication1.Remote.RetrofitClientLogIn;
+import com.google.gson.Gson;
+
+
+
+import java.util.ArrayList;
 import java.util.List;
 import io.reactivex.disposables.CompositeDisposable;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+
+
 public class Invoices extends AppCompatActivity implements InvoiceAdaptor.OnInvoiceListener{
     public interface RevealDetailsCallbacks {
         void getDataFromResult(List<InvoiceModel> list);
     }
+
+
 
     private RecyclerView recyclerView;
     private InvoiceAdaptor invoiceAdaptor;
@@ -26,24 +41,48 @@ public class Invoices extends AppCompatActivity implements InvoiceAdaptor.OnInvo
     CompositeDisposable compositeDisposable= new CompositeDisposable();
     RevealDetailsCallbacks callback;
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invoices);
         recyclerView = this.findViewById(R.id.invoiceRecycleView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        List<InvoiceModel> invoices = new ArrayList<>();
+        invoiceAdaptor = new InvoiceAdaptor(Invoices.this,invoices,Invoices.this);
+        recyclerView.setAdapter(invoiceAdaptor);
+
+
+
+        SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Gson gson = new Gson();
+        String json = mPrefs.getString("AccountInfo",null);
+        AccountModel account = gson.fromJson(json, AccountModel.class);
+
+
 
         this.callback = new RevealDetailsCallbacks() {
             @Override
             public void getDataFromResult(List<InvoiceModel> list) {
-                invoiceAdaptor = new InvoiceAdaptor(Invoices.this,list,Invoices.this);
-                recyclerView.setAdapter(invoiceAdaptor);
+                for(int i = 0; i< list.size(); i++)
+                {
+                    if(account.getAccountId() == list.get(i).getAccountId())
+                    {
+                        invoices.add(list.get(i));
+                        invoiceAdaptor.notifyDataSetChanged();
+                    }
+                }
             }
         };
         getInvoiceList(this, callback);
     }
+
+
 
     public void getInvoiceList(Context context, RevealDetailsCallbacks callback) {
         invoiceAPI = RetrofitClientLogIn.getInstance().create(APIInterfaces.class);
@@ -57,6 +96,8 @@ public class Invoices extends AppCompatActivity implements InvoiceAdaptor.OnInvo
                 }
             }
 
+
+
             @Override
             public void onFailure(Call<List<InvoiceModel>> call, Throwable t) {
                 call.cancel();
@@ -65,10 +106,14 @@ public class Invoices extends AppCompatActivity implements InvoiceAdaptor.OnInvo
         });
     }
 
+
+
     @Override
     public void onInvoiceListener(int position) {
         InvoiceDetails secondFragment = new InvoiceDetails();
     }
+
+
 
     @Override
     protected void onStop() {
