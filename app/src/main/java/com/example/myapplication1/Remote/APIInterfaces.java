cb6ac8f3d9eca2ac416e89e2a3671384f335c15f
miruna.cojocaru <servetelezewa34>
2020-11-05 17:45:02 +0200

    merge api

--- app/src/main/java/com/example/myapplication1/Remote/APIInterfaces.java
+++ app/src/main/java/com/example/myapplication1/Remote/APIInterfaces.java
@@ -19,18 +19,11 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.GET;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
-import retrofit2.http.PUT;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
-import retrofit2.http.Part;
-import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface APIInterfaces {
 
-    //@Multipart
     @GET("Index/GetIndexesByAccountId/{accountId}")
     Call<List<IndexModel>> getAllIndexis(@Path("accountId") int accountId);
 
@@ -45,7 +38,6 @@ public interface APIInterfaces {
 
     @POST("Index/AddIndexToAccountId")
     Call<ModelAddIndex> insertIndex(@Field("accountId") int accountId,@Field("addresstId") int addresstId,@Field("indexValue") float indexValue);
-
 
     @GET("InvoiceDetail/GetInvoiceDetails/{invoiceId}")
     Call<InvoiceDetailsModel> getInvoiceDetailsByInvoice(@Path("invoiceId") int invoiceId);
