d4f1c5f899d886466af093e9cd1824b4d7210b58
ileanazina <69892808+ileanazina@users.noreply.github.com>
2020-11-25 11:57:05 +0200

    xml for forms2

---
+++ app/src/main/java/com/example/myapplication1/Activities/FormAdaptor.java
@@ -0,0 +1,75 @@
+package com.example.myapplication1.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.myapplication1.Activities.Index.IndexAdaptor;
+import com.example.myapplication1.Model.IndexModel;
+import com.example.myapplication1.R;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public class FormAdaptor extends RecyclerView.Adapter<IndexAdaptor.ExempleViewHolder> {
+
+    private Context mContext;
+   private List<IndexModel> indexList;
+    private IndexAdaptor.OnIndexListener monIndexListener;
+
+    public FormAdaptor(Context mContext, List<IndexModel> mList, IndexAdaptor.OnIndexListener onIndexListener) {
+        this.mContext = mContext;
+        this.indexList = mList;
+        this.monIndexListener=onIndexListener;
+    }
+
+    @NonNull
+    @Override
+    public IndexAdaptor.ExempleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.form_row_view,parent,false);
+        return new FormAdaptor.ExempleViewHolder(v,monIndexListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FormAdaptor.ExempleViewHolder holder, int position) {
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return indexList.size();
+    }
+
+    public class ExempleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+//        public TextView tvIndexNr,tvIndexDate, tvIndexavr;
+//        IndexAdaptor.OnIndexListener onIndexListener;
+
+        public ExempleViewHolder(@NonNull View itemView, FormAdaptor.OnIndexListener onIndexListener) {
+            super(itemView);
+//            tvIndexNr = itemView.findViewById(R.id.indexValue);
+//            tvIndexDate = itemView.findViewById(R.id.indexDate);
+//            tvIndexavr = itemView.findViewById(R.id.indexAvr);
+//
+//            this.onIndexListener=onIndexListener;
+//            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onIndexListener.onIndexListener(getAdapterPosition());
+        }
+    }
+
+    public interface OnIndexListener {
+        void onIndexListener(int position);
+    }
+}
