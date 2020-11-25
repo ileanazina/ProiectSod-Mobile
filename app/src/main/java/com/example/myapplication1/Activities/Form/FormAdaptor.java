eaaffdf3bf9b37500160de52dbce7eb62a544dfe
ileanazina <69892808+ileanazina@users.noreply.github.com>
2020-11-25 12:50:42 +0200

    formAdaptor

--- app/src/main/java/com/example/myapplication1/Activities/Form/FormAdaptor.java
+++ app/src/main/java/com/example/myapplication1/Activities/Form/FormAdaptor.java
@@ -9,10 +9,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.myapplication1.Activities.Index.IndexAdaptor;
+import com.example.myapplication1.Model.DocumentDownloadModel;
 import com.example.myapplication1.Model.IndexModel;
+import com.example.myapplication1.Model.InvoiceModel;
 import com.example.myapplication1.R;
 
 import java.text.SimpleDateFormat;
@@ -21,55 +24,79 @@ import java.util.List;
 public class FormAdaptor extends RecyclerView.Adapter<FormAdaptor.ExempleViewHolder> {
 
     private Context mContext;
-   private List<IndexModel> indexList;
-    private OnIndexListener monIndexListener;
+    private List<DocumentDownloadModel> documentList;
+    private OnFormListener monFormListener;
 
-    public FormAdaptor(Context mContext, List<IndexModel> mList, OnIndexListener onIndexListener) {
+    public FormAdaptor(Context mContext, List<DocumentDownloadModel> mList, OnFormListener monFormListener) {
         this.mContext = mContext;
-        this.indexList = mList;
-        this.monIndexListener=onIndexListener;
+        this.documentList = mList;
+        this.monFormListener=monFormListener;
     }
 
     @NonNull
     @Override
     public ExempleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(mContext).inflate(R.layout.form_row_view,parent,false);
-        return new FormAdaptor.ExempleViewHolder(v,monIndexListener);
+        return new FormAdaptor.ExempleViewHolder(v,monFormListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ExempleViewHolder holder, int position) {
-
+        DocumentDownloadModel item = documentList.get(position);
+        holder.tvDocumentType.setText("Documentul" + item.getDocumentName());
     }
 
 
     @Override
     public int getItemCount() {
-        return indexList.size();
+        return documentList.size();
     }
 
     public class ExempleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-//        public TextView tvIndexNr,tvIndexDate, tvIndexavr;
-//        IndexAdaptor.OnIndexListener onIndexListener;
+        public TextView tvDocumentType;
+        public Button btndown, btnup;
+        FormAdaptor.OnFormListener onFormListener;
 
-        public ExempleViewHolder(@NonNull View itemView, FormAdaptor.OnIndexListener onIndexListener) {
+        public ExempleViewHolder(@NonNull View itemView, FormAdaptor.OnFormListener onFormListener) {
             super(itemView);
-//            tvIndexNr = itemView.findViewById(R.id.indexValue);
-//            tvIndexDate = itemView.findViewById(R.id.indexDate);
-//            tvIndexavr = itemView.findViewById(R.id.indexAvr);
-//
-//            this.onIndexListener=onIndexListener;
-//            itemView.setOnClickListener(this);
+            tvDocumentType = itemView.findViewById(R.id.documentType);
+            btndown = itemView.findViewById(R.id.download);
+            btnup = itemView.findViewById(R.id.upload);
+
+            btndown.setOnClickListener(this);
+            btnup.setOnClickListener(this);
+
+            this.onFormListener=onFormListener;
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            monIndexListener.onIndexListener(getAdapterPosition());
+
+            onFormListener.onFormListener(getAdapterPosition());
+            int position;
+            position = getAdapterPosition();
+            DocumentDownloadModel item = documentList.get(position);
+
+            switch (v.getId()) {
+                case R.id.download:
+                    downloadDoc(item);
+                    break;
+                case R.id.upload:
+                    uploadDoc(item);
+                    break;
+            }
+        }
+
+        private void uploadDoc(DocumentDownloadModel item) {
+        }
+
+        private void downloadDoc(DocumentDownloadModel item) {
         }
     }
 
-    public interface OnIndexListener {
-        void onIndexListener(int position);
+    public interface OnFormListener {
+         void onFormListener(int position);
     }
 }
